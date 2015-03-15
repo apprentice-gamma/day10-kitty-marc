454405cf292271cc697fe518a687846c42dfc9b4
Marc Nischan <marcnischan@gmail.com>
2015-03-14 23:07:22 -0400

    Adding updated code to fix faiiling test number nine

--- exercism/javascript/bob/bob.js
+++ exercism/javascript/bob/bob.js
@@ -9,19 +9,30 @@ Bob.prototype.hey = function(input) {
 
 	var sayToBob = input.trim();
 	
-	if(input = ""){
+	if(input == ""){
 		return "Fine. Be that way!";
-	} else if( sayToBob.match(/\d/) && !sayToBob.match(/[a-zA-Z]/) ){
+	} else if( sayToBob.match(/\D/) && !sayToBob.match(/[a-zA-Z]/) && sayToBob.charAt(sayToBob.length-1) !== "?"){
+		//only numbers
 		return "Whatever.";
-	} else if (sayToBob === sayToBob.toUpperCase() ){
-        return 'Whoa, chill out!';
-    } else if (sayToBob.charAt(sayToBob.length-1) === "?"){
-        return 'Sure.';
+	} else if (sayToBob.charAt(sayToBob.length-1) === "?"){
+        //asking a question
+        if(!sayToBob.match(/[a-zA-Z]/) && sayToBob.match(/\D/)){
+        	//number as a question
+        	return 'Sure.';	
+        }
+        else if(sayToBob === sayToBob.toUpperCase()){
+        	//forceful question
+        	return 'Whoa, chill out!';
+        } else {
+        	return 'Sure.';
+        }
+	} else if (sayToBob === sayToBob.toUpperCase()){
+		return 'Whoa, chill out!';
+	} else if( sayToBob.match(/\d/) && !sayToBob.match(/[a-zA-Z?]/) ){
+		return "Whatever.";
 	} else {
         return "Whatever.";
     }
 };
 
 module.exports = Bob;
-
-
